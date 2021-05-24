1c89b3698bd987378544c23a749fc762d99e6eac
Erica Winne <erica.winne@powerhrg.com>
2021-05-24 13:45:26 -0400

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+    function upperCaseCustomerName() {
+        customerName = 'BOB'
+    }
+    function setBestCustomer(){
+        bestCustomer = 'not bob'
+    }
+    function overwriteBestCustomer(){
+        bestCustomer = 'maybe bob'
+        return bestCustomer
+    }
+    function changeLeastFavoriteCustomer(){
+    }
+    const leastFavoriteCustomer = 'Benny'
+
+    function changeLeastFavoriteCustomer(){   
+        leastFavoriteCustomer = 'Erica'
+        return leastFavoriteCustomer
+    }
+
